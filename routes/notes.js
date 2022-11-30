7628a8fbc511da7e72e20f5d069c8d94c909af1c
Matt Thurn <matt.thurn@icloud.com>
2022-11-29 23:30:36 -0700

    added the notes routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,62 +1,52 @@
 const notesRouter = require('express').Router();
 const uuid =require('../helpers/uuid');
 const fs = require('fs');
+const dbData = require('../db/db.json');
 
 notesRouter.get('/', (req, res) => {
-    // Send a message to the client
-    res.status(200).json(`${req.method} request received to get reviews`);
-  
-    // Log our request to the terminal
-    console.log(`${req.method} request received to get reviews`);
-  });
-
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    const newData = JSON.parse(data);
+    console.log(newData)
+    res.status(200).json(newData);
+  })
+});
 
 notesRouter.post('/', (req, res) => {
     // Log that a POST request was received
-    const response = res.json;
+    //const response = res.json;
     // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
-  
-    // If all the required properties are present
-    if (title && text) {
-      // Variable for the object we will save
-      const newNote = {
-        title,
-        text,
-        note_id: uuid(),
-      };
-  
-      // Obtain existing reviews
-      fs.readFile('./db/notes.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          // Convert string into JSON object
-          const reviews = JSON.parse(data);
-  
-          // Add a new review
-          reviews.push(newNote);
-  
-          // Write updated reviews back to the file
-          fs.writeFile(
-            './db/notes.json',
-            JSON.stringify(reviews, null, 4),
-            (writeErr) =>
+	const { title, text } = req.body;
+
+	if (title && text) {
+		const newNote = {
+			title,
+			text,
+			note_id: uuid(),
+		};
+
+		fs.readFile("./db/db.json", "utf8", (err, data) => {
+			if (err) {
+				console.error(err);
+			} else {
+				const reviews = JSON.parse(data);
+
+				reviews.push(newNote);
+
+				fs.writeFile(
+					"./db/db.json",
+					JSON.stringify(reviews, null, 4),
+					(writeErr) =>
               writeErr
                 ? console.error(writeErr)
                 : console.info('Success')
-          );
-        }
-      });
-  
-  
-      console.log(response);
-      res.status(201).json(response);
-    } else {
-      res.status(500).json('Error: posting review');
-    }
-  });
-  
+				);
+			}
+		});
 
+		res.status(201).json("SUCCESS");
+	} else {
+		res.status(500).json("Error: posting review");
+	}
+});
 
 module.exports = notesRouter;
